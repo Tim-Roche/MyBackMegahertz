ecfc9608e46c8bed16dcd6db6ab81635617905d2
Timothy Roche <kc2vsa@gmail.com>
2019-04-05 17:19:19 -0400

    workarround to not have to download quartus 64

--- computer.v
+++ computer.v
@@ -17,7 +17,7 @@ controlUnit cu(clock, reset, IR, status, controlWord, k);
 datapath   dp (clock, reset, controlWord, k, status, IR, mem_data, mem_address, r0, r1, r2, r3, r4, r5, r6, r7);
 
 RAM_64bit ram (clock, mem_address, mem_data, mem_cs, mem_write_en, mem_read, size);
-defparam ram.ADDR_WIDTH = 32;
+defparam ram.ADDR_WIDTH = 16;
 
 ROM   prgmMem (mem_address, mem_data, chip_select, output_enable); 
 
