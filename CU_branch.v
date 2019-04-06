46aa5e6f03736104c6292dc6e7aac937cb5a887b
Timothy Roche <kc2vsa@gmail.com>
2019-04-05 21:54:11 -0400

    WIP adding branchCU

--- CU_branch.v
+++ CU_branch.v
@@ -1,3 +1,50 @@
+module CU_branch(IR, state, status, NS, k_mux, controlWord);
+parameter CUL = 36;
+input [3:0] state;
+input [3:0] status; 
+input [31:0] IR;
+output [2:0] k_mux;
+output [2:0] NS;
+output [CUL:0] controlWord;
+wire [10:0] opcode = IR[31:21];
+wire [2:0] trimOp1 = opcode[31:29];
+wire [1:0] trimOp0 = opcode[25:24];
+
+wire UNUSED7, BR, CB, BL, UNUSED3, bcond, UNUSED1, B;
+Decoder3to8 opToSignal ({UNUSED7, BR, CB, BL, UNUSED3, bcond, UNUSED1, B},trimOp1, 1'b1);
+wire CBZ  = CB &~trimOp0[1]&~trimOp0[0];
+wire CBNZ = CB &~trimOp0[1]& trimOp0[0];
+
+wire EX0 = (state == 4'b0001) ? 1'b1 : 1'b0;
+wire EX1 = (state == 4'b0010) ? 1'b1 : 1'b0;
+
+assign NS = (BR)     ? 4'b0000 : 
+            (EX0&~BR)? 4'b0010 :
+				(EX1)    ? 4'b0000;
+
+wire [4:0] FS           = 5'b00100; //OR
+wire [4:0] SA           = 5'd31;
+wire [4:0] SB           = IR[4:0];
+wire [4:0] DA           = 5'd28; //RL 
+
+wire w_reg              = EX0&BL;
+wire C0                 = 1'b0;
+wire [1:0] mem_cs       = 2'b00;
+wire B_Sel              = 1'b0;
+wire mem_write_en       = 1'b0;
+wire IR_load            = 1'b0;
+wire status_load        = CB; 
+wire [1:0] size         = 2'b00;
+wire add_tri_sel        = 1'b0;
+wire [1:0] data_tri_sel = 2'b00;
+wire PC_sel             = 1'b0;
+wire [1:0] PC_FS        = 2'b01;
+wire [2:0] k_mux        = 3'b000;
+
+wire [CUL:0] controlWord = {FS, SA, SB, DA, w_reg, C0, mem_cs, B_Sel, mem_write_en, IR_load, status_load, size, add_tri_sel, data_tri_sel, PC_sel, PC_FS};
+
+endmodule
+
 module CU_branch(state, status, opcode, SA_in, SB_in, DA_in, controlWord);
 //----DP_reg_sel
 input [3:0] state;
